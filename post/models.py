13a4dd1391397fcaeab5f1655286e931ac09ec0f
ym4199 <ym4199@gmail.com>
2017-06-21 19:26:43 +0900

    model 구성

--- post/models.py
+++ post/models.py
@@ -1,7 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
 
+User = get_user_model()
+
 
 class Post(models.Model):
-    pass
+    author = models.ForeignKey(User)
+    photo = models.ImageField(upload_to='post', blank=True)
+    comment = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    modify_date = models.DateTimeField(auto_now=True)
--- post/views.py
+++ post/views.py
@@ -1,10 +1,9 @@
-from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
-User=get_user_model()
+
 
 def post_list(request):
-    return HttpResponse('test')
+    pass
