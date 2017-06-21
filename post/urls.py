1fa15fb558294c20bcc0392db5960c1fd9e34398
ym4199 <ym4199@gmail.com>
2017-06-21 20:28:52 +0900

    post detail 구현 해당 페이지에서 delete 구현

--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+
 from . import views
 
-urlpatterns =[
+urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^(?P<post_pk>\d+)/delete', views.post_delete, name='post_delete'),
+    url(r'^(?P<post_pk>\d+)/detail', views.post_detail, name='post_detail'),
 ]
--- post/views.py
+++ post/views.py
@@ -11,8 +11,17 @@ def post_list(request):
     }
     return render(request, 'post_list.html', context)
 
+
 def post_delete(request, post_pk):
     if request.method == 'POST':
         form = Post.objects.get(pk=post_pk)
         form.delete()
         return redirect('post_list')
+
+
+def post_detail(request, post_pk):
+    posts = Post.objects.get(pk=post_pk)
+    context = {
+        'post':posts,
+    }
+    return render(request, 'post_detail.html', context)
