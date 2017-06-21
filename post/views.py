93f2a8274dc3c59d689a1669ce85a0c6d6cfbb7a
ym4199 <ym4199@gmail.com>
2017-06-21 19:59:01 +0900

    postlist html 표현

--- post/views.py
+++ post/views.py
@@ -1,9 +1,13 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-
+from post.models import Post
 
 
 def post_list(request):
-    pass
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'post_list.html', context)
+
